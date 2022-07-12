4f7b844582f9e3e5888ebfd4b03046e3d9304132
Matteo Susanna <matteo2784@gmail.com>
2022-07-12 15:48:23 +0200

    Inizio palindromi

---
+++ palindromi/js/main.js
@@ -0,0 +1,67 @@
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+
+ */
+
+
+let parola = prompt('Inserisci una parola');
+
+
+parola = palindroma(parola);
+
+
+if(verifica = 'palindroma'){
+    alert('la parola è palindroma')
+}else {
+    alert('la parola non è palindroma')
+}
+
+
+
+
+function palindroma (parola){
+
+    let myArray = parola.split('');
+    console.log(myArray);
+
+    myArray.reverse();
+    console.log(myArray);
+
+
+    let verifica = '';
+
+    for( let i = myArray.length; i > 0; i--){
+
+        if(myArray[i] == parola[i]){
+            pali = "palindroma";
+        }else{
+            pali = "nopalindroma";
+        }
+
+    }
+
+    return verifica;
+
+
+
+
+
+
+/*
+    let pali = '';
+
+    if(myArray.reverse == parola.split){
+        pali = "palindroma";
+    }else{
+        pali = "nopalindroma";
+    }
+
+
+    return pali;
+
+*/
+}
+
